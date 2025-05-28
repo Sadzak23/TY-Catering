a618532a0e7ff631e06e6a98d451ad0e533cf4e4
Sadzak23 <sadzak23@gmail.com>
2025-05-29 00:26:28 +0200

    Create CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { AboutUs } from "@/sections/AboutUs";
 // import { Portfolio } from "@/sections/Portfolio";
 import { Calculator } from "@/sections/Calculator";
 import { Contact } from "@/sections/Contact";
+import { CallToAction } from "@/sections/CTA";
 
 const Home: FC = () => (
   <>
     <HeroSection />
     <AboutUs />
     {/* <Portfolio /> */}
+    <CallToAction />
     <Calculator />
     <Contact />
   </>
---
+++ src/sections/CTA.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/Button";
+import type { FC } from "react";
+
+export const CallToAction: FC = () => (
+  <section id="cta">
+    <h2>{`Let Us Help Turn Your Event 
+into an Unforgettable Experience.`}</h2>
+    <Button label="Start your journey" />
+  </section>
+);
