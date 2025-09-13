b2d5fcbff6e4dbb371ddaadf2140e84a445badd7
Sadzak23 <sadzak23@gmail.com>
2025-09-13 20:53:22 +0200

    Update AboutUs to match the rest of the sections

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -4,17 +4,19 @@ import img from "@/assets/img/aboutUs.webp";
 
 export const AboutUs: FC = () => (
   <section id="about">
-    <article>
-      <h2>About us</h2>
-      <p>
-        {`With over 15 years of experience, Truly Yours Catering brings expertise
+    <h2>About us</h2>
+    <div className="content">
+      <article>
+        <p>
+          {`With over 15 years of experience, Truly Yours Catering brings expertise
         and care to every event we plan and execute. Whether you're envisioning
         an elegant affair, a lively celebration, or something entirely unique,
         simply share your vision—and then relax. Our dedicated administrative
         and event teams will handle every detail to ensure a seamless and
         memorable experience.`}
-      </p>
-    </article>
-    <Image aria-hidden src={img} alt="About us" width={500} />
+        </p>
+      </article>
+      <Image aria-hidden src={img} alt="About us" width={500} />
+    </div>
   </section>
 );
